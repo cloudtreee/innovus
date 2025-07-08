0805c11b5003e04fca271c8cf85ddafe66a48d99
cloudtreee <166366991+cloudtreee@users.noreply.github.com>
2025-07-08 23:21:13 +0800

    Update run.tcl

--- scr/run.tcl
+++ scr/run.tcl
@@ -1,1 +1,10 @@
-source
+
+
+
+
+######################init_design
+source   $scr_dir/init.tcl
+######################fp
+source   $scr_dir/fp.tcl
+#######################place2postR way1
+source   $scr_dir/place2postR.tcl
